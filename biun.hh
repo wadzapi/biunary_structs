0e445ef4fe7ba7191a984641cbdfecf36d60e059
WadZapi <wadzapi@gmail.com>
2013-07-14 14:39:19 +0400

    datastruct class added

--- biun.hh
+++ biun.hh
@@ -1,6 +1,15 @@
 #ifndef BIUN_H_
 #define BIUN_H_
+/* Basic data structure - classic binary tree */
 
+struct tree_node_base {
+    tree_node_base* Left;
+    tree_node_base* Right;
+};
 
+template <class Value>
+struct tree_node : public tree_node_base {
+    Value value_;
+};
 
-#endif //BIUN_H
+#endif //BIUN_H_
---
+++ data_struct.hh
@@ -0,0 +1,23 @@
+#ifndef DATA_STRUCT_H_
+#define DATA_STRUCT_H_
+
+#include "memory_heap.h"
+#include "biun.h"
+
+/* Class-container for logical and phisical storage                           */
+/* functions to set phisical data, and to link for logical structure data */
+
+template <class Tp>
+class DataStruct {
+    private:
+        MemoryHeap<Tp> physical_data_;
+        MemoryHeap<tree_node<Tp> > logical_data_;
+    public:
+        DataStruct();
+        ~DataStruct();
+        void Allocate(size_t phys_size, size_t logic_size);
+        void Free();
+
+};
+
+#endif DATA_STRUCT_H_
